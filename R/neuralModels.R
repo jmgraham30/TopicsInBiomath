8a9eabd3b24c7f64498f5b277aad6b6f4a45fda2
Jason M Graham <prof.jason.m.graham@gmail.com>
2022-04-18 14:29:06 -0400

    update
    update

--- R/neuralModels.R
+++ R/neuralModels.R
@@ -1,5 +1,6 @@
 source("R/gatingVariables.R")
 
+library(rootSolve)
 
 HH <- function (t, y, parameters) {
   with(as.list(c(y,parameters)),{
@@ -45,7 +46,8 @@ FN <- function (t, y, parameters) {
 }
 
 HH_v_null <- function(v,n,parameters){
-  with(as.list(c(y,parameters)),{
+  with(as.list(c(v,n,parameters)),{
+    
     rhs <- gna*m_inf(v)^3*(0.83-n)*(vna - v) + gk*n^4*(vk - v) + gl*(vl - v) + iext
     
     return(rhs)
@@ -55,3 +57,17 @@ HH_v_null <- function(v,n,parameters){
 
 HH_parameters <- list(C=1,gk=36,gna=120,gl=0.3,vk=-82,vna=45,vl=-59,iext=10)
 
+
+compute_v_null <- function(v_val){
+  if (v_val < -81.52){
+    n_val <- 1.0
+  }else if(v_val >= -81.52 && v_val <= 44.78 ){
+    f <- function(n){HH_v_null(n,v=v_val,parameters=HH_parameters)}
+    n_val <- uniroot(f,interval=c(0,1))$root 
+  }else {
+    n_val <- 0.0
+  }
+  
+  return(n_val)
+  
+}
